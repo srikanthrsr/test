6e1e965098761117a67710a0a013aeb50b1bba00
madwarakshay <akshay.madwar@einfochips.com>
2019-10-21 14:39:46 +0530

    commit through terminal

--- welcome.sh
+++ welcome.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-greeting="* * * T E S T I N G  __ EVERY      +++++     MINUTE    1111111111111  22222222222* * * "
+greeting="Welcome to JENKINS TEST "
 user=$(whoami)
 day=$(date +%A)
 
-echo "$greeting back $user! Today is $day, which is the best day of the entire week!"
+#echo "$greeting back $user! Today is $day, which is the best day of the entire week!"
 #echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
