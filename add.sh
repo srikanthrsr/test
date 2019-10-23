6ae95e3bc90b394cfb0cb4e045e5189c1b45a3d8
srikanthrsr <srikanth.reddy@einfochips.com>
2019-10-23 16:35:32 +0530

    dd

--- add.sh
+++ add.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#new=`expr $1 + $2 `
+#new=` expr $1 + $2 `
 
 read var1
 read var2
-new=`expr $var1 + $var2 `
+new=` expr $var1 + $var2 `
 
 echo "$new"
--- ex_.sh
+++ ex_.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo "enter 1st num"
-read num1
+#echo "enter 1st num"
+#read num1
 
-echo "enter 2nd num"
-read num2
+#echo "enter 2nd num"
+#read num2
+num1="3"
+num2="5"
 
-echo "$num1 $num2"
+#echo "$num1"
+#echo "$num2"
+printf "$num1 \n$num2\n"
+
